a354ebfa4b88eead89b3afb37140a53781670ef0
tarekabdelsalam <tarekrs@gmail.com>
2014-11-18 16:34:16 -0800

    First commit

---
+++ src/main/java/com/shekhar/wordgame/App.java
@@ -0,0 +1,13 @@
+package com.shekhar.wordgame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
